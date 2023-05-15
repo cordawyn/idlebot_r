d81aa9ef906eff105e012affbc47478e75cbd18b
cordawyn <slava.kravchenko@gmail.com>
2023-05-15 10:45:13 +0200

    deserializing the timestamp

--- src/commands/idle.rs
+++ src/commands/idle.rs
@@ -1,6 +1,7 @@
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::prelude::GuildId;
 use serenity::model::prelude::interaction::application_command::CommandDataOption;
+use serenity::model::timestamp::Timestamp;
 use sled::Db;
 
 pub fn run(_options: &[CommandDataOption], db: &mut Db, gid: &GuildId) -> String {
@@ -9,9 +10,12 @@ pub fn run(_options: &[CommandDataOption], db: &mut Db, gid: &GuildId) -> String
         // ordered by idle time (most idle at the top).
         for rec in tree.iter() {
             let (k, v) = rec.unwrap();
-            println!("Record: {:#?} {:#?}",
+            let bv = <[u8; 8]>::try_from(v.as_ref()).expect("oops");
+            if let Ok(ts) = Timestamp::from_unix_timestamp(i64::from_be_bytes(bv)) {
+                println!("Record: {:#?} {:#?}",
                 k.to_vec(),
-                v.to_vec());
+                ts);
+            }
         }
     }
     // Output the results as a string (message content).
