33fb3e39b63ba6520783fbca24f016ea05e45197
cordawyn <slava.kravchenko@gmail.com>
2023-06-02 11:24:36 +0200

    producing a sorted list of idle users

--- src/commands/idle.rs
+++ src/commands/idle.rs
@@ -3,23 +3,43 @@ use serenity::model::prelude::GuildId;
 use serenity::model::prelude::interaction::application_command::CommandDataOption;
 use serenity::model::timestamp::Timestamp;
 use sled::Db;
+use std::collections::BTreeMap;
+use std::time::Duration;
+use human_repr::HumanDuration;
+
 
 pub fn run(_options: &[CommandDataOption], db: &mut Db, gid: &GuildId) -> String {
+    let mut sorted_authors: BTreeMap<i64, String> = BTreeMap::new();
+
     if let Ok(tree) = db.open_tree(gid.to_string()) {
-        // TODO: Read the DB and retrieve a list of users,
-        // ordered by idle time (most idle at the top).
-        for rec in tree.iter() {
-            let (k, v) = rec.unwrap();
-            let bv = <[u8; 8]>::try_from(v.as_ref()).expect("oops");
-            if let Ok(ts) = Timestamp::from_unix_timestamp(i64::from_be_bytes(bv)) {
-                println!("Record: {:#?} {:#?}",
-                k.to_vec(),
-                ts);
+        if let Ok(authors) = db.open_tree("authors") {
+            for rec in tree.iter() {
+                let (k, v) = rec.unwrap();
+    
+                let ts_bv = <[u8; 8]>::try_from(v.as_ref()).unwrap();
+                let unix_ts = i64::from_be_bytes(ts_bv);
+                let aid_bv = <[u8; 8]>::try_from(k.as_ref()).unwrap();
+                // let aid = UserId::try_from(u64::from_be_bytes(aid_bv)).unwrap();
+                let name = match authors.get(&aid_bv).unwrap() {
+                    Some(n) => String::from_utf8(n.to_vec()).unwrap(),
+                    None => String::from("Unknown")
+                };
+    
+                sorted_authors.insert(unix_ts, name);
             }
         }
     }
+
+    let mut aus = String::new();
+    for (unix_ts, name) in sorted_authors.iter() {
+        let ts = Timestamp::from_unix_timestamp(*unix_ts).unwrap();
+        let delta = Timestamp::now().unix_timestamp() - ts.unix_timestamp();
+        let entry = format!("* {} ({})\n", name, Duration::from_secs(delta.try_into().unwrap()).human_duration().to_string());
+        aus.push_str(&entry);
+    }
+
     // Output the results as a string (message content).
-    "List of idle users:".to_string()
+    format!("List of idle users:\n{aus}")
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
